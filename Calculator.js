ed5362dbbcf953ed2fca2978d080032cc990faf0
rajbaner <142020902+rajbaner@users.noreply.github.com>
2023-10-14 08:54:50 -0700

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import './Calculator.css';
+
+const Calculator = () => {
+  const [display, setDisplay] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleButtonClick = (value) => {
+    if (value === '=') {
+      try {
+        setResult(eval(display));
+      } catch (error) {
+        setResult('Error');
+      }
+    } else if (value === 'C') {
+      setDisplay('');
+      setResult('');
+    } else {
+      setDisplay(display + value);
+    }
+  };
+
+  const buttons = [
+    '7', '8', '9', '/',
+    '4', '5', '6', '*',
+    '1', '2', '3', '-',
+    '0', '.', '=', '+',
+    'C'
+  ];
+
+  return (
+    <div className="calculator">
+      <div className="display">
+        <div className="display-screen">
+          <span className="result">{result}</span>
+          <span className="input">{display}</span>
+        </div>
+      </div>
+      <div className="buttons">
+        {buttons.map((button, index) => (
+          <button key={index} onClick={() => handleButtonClick(button)}>
+            {button}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
